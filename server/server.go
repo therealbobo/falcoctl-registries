2f54543f445f5939632894f2631b83aeaa6a1af4
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-11-10 19:34:49 +0100

    retrieving authorization grant as code
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-oauth2/oauth2/v4/errors"
 	"github.com/go-oauth2/oauth2/v4/manage"
@@ -21,12 +22,24 @@ func main() {
 	clientStore.Set("000000", &models.Client{
 		ID:     "000000",
 		Secret: "999999",
-		Domain: "http://localhost",
+		Domain: "http://localhost:9095/callback",
 	})
 	manager.MapClientStorage(clientStore)
 
+	refreshTokenConfig := &manage.RefreshingConfig{
+		AccessTokenExp:     time.Minute,
+		RefreshTokenExp:    time.Hour * 24,
+		IsGenerateRefresh:  true,
+		IsResetRefreshTime: false,
+		IsRemoveAccess:     false,
+		IsRemoveRefreshing: false,
+	}
+	manager.SetRefreshTokenCfg(refreshTokenConfig)
+
 	srv := server.NewDefaultServer(manager)
-	srv.SetAllowGetAccessRequest(true)
+
+	srv.SetAllowGetAccessRequest(false) // it was true before
+
 	srv.SetClientInfoHandler(server.ClientFormHandler)
 
 	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
@@ -38,7 +51,12 @@ func main() {
 		log.Println("Response Error:", re.Error.Error())
 	})
 
+	srv.SetUserAuthorizationHandler(func(w http.ResponseWriter, r *http.Request) (string, error) {
+		return "id", nil
+	})
+
 	http.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("received /authorize request")
 		err := srv.HandleAuthorizeRequest(w, r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
